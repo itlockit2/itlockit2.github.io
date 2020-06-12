2cba0006031cba7f6034b38b9e0b5446b24ae3f9
SonghoYun <itlockit@gmail.com>
2020-06-12 13:48:19 +0900

    first deploy

---
+++ pages/get-started.js
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <>
+      <div className="container">
+        <h1> 시작하기</h1>
+        <p>npm을 통해 CLI를 설치합니다.</p>
+        <code>npm i -g wehealed-nextjs-cli</code>
+      </div>
+    </>
+  );
+};
